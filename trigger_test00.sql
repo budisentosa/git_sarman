de063357775834aac2c49b733e18e6a584ead01d
budisentosa <budi@avasoft.co>
2021-11-02 10:45:07 +0700

    test00

---
+++ trigger_test00.sql
@@ -0,0 +1,44 @@
+CREATE DEFINER=`root`@`%` TRIGGER `test00` AFTER INSERT ON `barcodegen` FOR EACH ROW begin
+	declare i int default 0;
+	declare depanBarcode varchar(11);
+	declare blkBarcode varchar(11);
+	declare noBarcode int;
+
+	set depanBarcode = concat(new.nmpt, new.idcodetr, new.stokyear, '%');
+	select n.nobarcode 
+	into depanBarcode
+	from barcode n 
+	where n.nobarcode like depanBarcode 
+	order by n.nobarcode desc 
+	limit 1;
+	if (char_length(depanBarcode) = 5) then
+		begin
+			set i = 0;
+			set depanBarcode = concat(new.nmpt, new.idcodetr, new.stokyear); 
+			set blkBarcode = '00000';
+			set noBarcode = 0;
+		end;
+	else
+		begin
+			set blkBarcode = right(depanBarcode,5);
+			set noBarcode = right(depanBarcode,5);
+			set depanBarcode = concat(new.nmpt, new.idcodetr, new.stokyear); 
+		end;
+	end if;
+
+	simple_loop : loop
+		set i = i+1;
+		set noBarcode = noBarcode + 1;
+		set blkBarcode = noBarcode;
+		while (char_length(blkBarcode) < 5) do
+			set blkBarcode = concat("0", blkBarcode);
+		end while;
+		insert into barcode (idcodetr, stkyear, nobarcode, gen_date, status, username, idBarcodeGen)
+		values (new.idcodetr, new.stokyear, (concat(depanBarcode, blkBarcode)), NOW(), 'C', new.username, new.id);
+
+		if i = new.jml_gen then
+			leave simple_loop;
+		end if;
+
+	end loop simple_loop;
+end
